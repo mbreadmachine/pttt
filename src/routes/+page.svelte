f7c5f6d9e88414645a6a27fae4db414db353236d
SmallWebDev <krasznimarci@gmail.com>
2024-10-27 12:41:01 +0100

    made fetching functional, continuing tomorrow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,24 @@
 <script>
-	import SvelteMarkdown from 'svelte-markdown';
+	import { Carta, Markdown } from "carta-md"
+	import DOMPurify from "isomorphic-dompurify";
 	import Button from './../lib/components/ui/button/button.svelte';
+
+	let carta = new Carta({
+		sanitizer: DOMPurify.sanitize,
+	});
 
 	export let data
 </script>
-<Button on:click={() => console.log('click')}>Test button</Button>
+
+<svelte:head>
+	<title>PTTT - Home</title>
+</svelte:head>
+<Button onclick={() => console.log('click')}>Test button</Button>
 
 <!-- id, name, alsoknown, country, type, body -->
 {#each data.results as place}
 	<p>{place.name}</p>
 	<div class="border border-black markdown">
-		<SvelteMarkdown source={place.body} />
+		<Markdown {carta} value={place.body} />
 	</div>
 {/each}
